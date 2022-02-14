b4f3d392528835e9cce7fe4c53c573e00a3de2ef
Emil André Johannessen <emilaj@netcompany.com>
2022-02-14 10:48:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <!-- Must have the app property -->
+    <v-app-bar app></v-app-bar>
+
+    <v-container> Hello World </v-container>
+  </v-app>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class HelloWorld extends Vue {
+  // Declared as component data
+  message = "Hello World!";
+  message = "Hello World!";
+}
+</script>
+
+<style lang="scss"></style>
